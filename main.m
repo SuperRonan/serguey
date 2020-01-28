1c507f4dd915ab438cc8960830377aa874ecb8ef
Ronan Cailleau <ro.cailleau@gmail.com>
2020-01-28 15:56:35 +0100

    Added load channels function that handles chennels of different sizes

--- main.m
+++ main.m
@@ -1,20 +1,13 @@
-image_folder = 'data/alleia/';
+folder = 'data/factory/';
 
-R = imread(strcat(image_folder, 'r.png'));
-G = imread(strcat(image_folder, 'g.png'));
-B = imread(strcat(image_folder, 'b.png'));
-
-channels = {R, G, B};
-for i=1:3
-    channel = channels{i};
-    channel = channel(:, :, i);
-    channels{i} = channel;
-end
+channels = load_channels(folder);
 
 channels{4} = cat(3, channels{1}, channels{2}, channels{3});
 
 figure;
 for i=1: 4
-    subplot(1, 4, i);
+    subplot(2, 2, i);
     imshow(channels{i});
 end
+
+imwrite(channels{4}, strcat(folder, 'rgb.png'));
