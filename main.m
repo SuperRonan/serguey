ed8d58248dc6155dbe815c97fafc44436583d333
Glenn <glenn.kerbiriou@free.fr>
2020-01-29 22:52:53 +0100

    correct resizing (need uint8 images)

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 clear all; clf;
-folder = 'data/church/';
+folder = 'data/khan/';
 
 channels = load_channels(folder);
 
@@ -8,14 +8,14 @@ channels{4} = cat(3, channels{1}, channels{2}, channels{3});
 
 figure;
 
-for i=1: 4
+for i=1:4
     subplot(2, 2, i);
     imshow(channels{i});
 end
 
 imwrite(channels{4}, strcat(folder, 'rgb_naive.png'));
 
-pyramids = compute_pyramids(channels, 6);
+pyramids = compute_pyramids(channels, 10);
 [p_g, q_g, p_b, q_b] = recalage_IM(pyramids);
 
 channels{4} = cat(3, channels{1}, translation(channels{2}, p_g, q_g), translation(channels{3}, p_b, q_b));
