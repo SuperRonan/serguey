2d8bf1cc694713aa1ef5c36f17256f32ea76f2bc
Glenn <glenn.kerbiriou@free.fr>
2020-01-30 11:24:26 +0100

    pyramids contruction with minimum resolution arg

--- compute_pyramids.m
+++ compute_pyramids.m
@@ -1,10 +1,23 @@
-function pyramids = compute_pyramids(channels, depth)
-    pyramids = {}
+function pyramids = compute_pyramids(channels, arg, option)
+    pyramids = {};
+    
     for i = 1:3
         pyramids{i}{1} = channels{i};
-        for d = 2:depth
-            pyramids{i}{d} = impyramid(pyramids{i}{d-1}, 'reduce');
+        
+        if (option == 'minres')
+            M = min(size(channels{i}));
+            d = 2;
+            while M > arg
+                pyramids{i}{d} = impyramid(pyramids{i}{d-1}, 'reduce');
+                M = min(size( pyramids{i}{d}));
+                d = d+1;
+            end 
+        else
+            for d = 2:arg
+                pyramids{i}{d} = impyramid(pyramids{i}{d-1}, 'reduce');
+            end
         end
+
     end
 end
 
