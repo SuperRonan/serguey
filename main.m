0fc53b7ecb3a363a115db4cbd1a6c5b3052f57c9
Ronan Cailleau <ro.cailleau@gmail.com>
2020-01-31 12:18:07 +0100

    Added cropping to the main

--- main.m
+++ main.m
@@ -1,8 +1,11 @@
 clear all; clf;
-folder = 'data/khan/';
+folder = 'data/church/';
 
 channels = load_channels(folder);
-
+cropped_channels = channels;
+for i = 1:3
+    cropped_channels{i} = crop(channels{i}, 0.6);
+end
 
 channels{4} = cat(3, channels{1}, channels{2}, channels{3});
 
@@ -11,7 +14,7 @@ channels{4} = cat(3, channels{1}, channels{2}, channels{3});
 
 imwrite(channels{4}, strcat(folder, 'rgb_naive.png'));
 
-pyramids = compute_pyramids(channels, 10, 'minres');
+pyramids = compute_pyramids(cropped_channels, 50, 'minres');
 [p_g, q_g, p_b, q_b] = recalage_IM(pyramids);
     
 channels{4} = cat(3, channels{1}, translation(channels{2}, p_g, q_g), translation(channels{3}, p_b, q_b));
@@ -22,6 +25,12 @@ figure;
 for i=1:4
     subplot(2, 2, i);
     imshow(channels{i});
+end
+
+figure;
+for i=1:3
+    subplot(2, 2, i);
+    imshow(cropped_channels{i});
 end
 
 
