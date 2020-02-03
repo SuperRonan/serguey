1aedab0b9f877928a49a4fcbed444b0225cdde96
Ronan Cailleau <ro.cailleau@gmail.com>
2020-02-03 14:50:08 +0100

    Added display of the cropped image

--- main.m
+++ main.m
@@ -17,7 +17,7 @@ pyramids = compute_pyramids(cropped_channels, 50, 'minres');
 [p_g, q_g, p_b, q_b] = recalage_IM(pyramids);
 
 channels{4} = cat(3, channels{1}, translation(channels{2}, p_g, q_g), translation(channels{3}, p_b, q_b));
-
+cropped_channels{4} = cat(3, cropped_channels{1}, translation(cropped_channels{2}, p_g, q_g), translation(cropped_channels{3}, p_b, q_b));
 imwrite(channels{4}, strcat(folder, 'rgb_recale_IM.png'));
 
 figure;
@@ -27,7 +27,7 @@ for i=1:4
 end
 
 figure;
-for i=1:3
+for i=1:4
     subplot(2, 2, i);
     imshow(cropped_channels{i});
 end
